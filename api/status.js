b2f08977e033b2eb6e2d69e384e9416450f720ac
estanho <estanhoeu@gmail.com>
2025-08-13 01:26:23 -0300

    fix..

--- api/status.js
+++ api/status.js
@@ -1,6 +1,5 @@
 export default async function handler(req, res) {
   const serverIP = process.env.SERVER_IP;
-
   const response = await fetch(`https://api.mcsrvstat.us/3/${serverIP}`);
   const data = await response.json();
 
