e6d7366fc03d2423b9953ee528500563377288ee
estanho <estanhoeu@gmail.com>
2025-08-19 18:24:10 -0300

    feat: add update

--- src/data/updates.ts
+++ src/data/updates.ts
@@ -12,9 +12,9 @@ export const updates: updateInterface[] = [
     date: "19/08/2025",
     changes: [
       {
-        title: "✲ Inauguração do servidor?",
-        description: "Será que tudo vai funcionar?",
-        dependency: ["eis a questão"],
+        title: "✲ Inauguração do servidor",
+        description: "Uhulll",
+        dependency: [""],
       },
     ],
   },
