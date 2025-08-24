946f567887d14ac97c0d198ab1fa2c9cdcb479fd
estanho <estanhoeu@gmail.com>
2025-08-24 03:17:48 -0300

    feat: update download link

--- src/data/download.ts
+++ src/data/download.ts
@@ -2,5 +2,5 @@ export const download = {
   download_url:
     "https://drive.google.com/file/d/10N_d9LkpnF6Pte93E3o8zCd4whe1-oSm/view?usp=sharing",
   last_update_url:
-    "https://drive.google.com/file/d/1FKaPQPj6t8EkkAThAQIK-0LyaK5s-0dE/view?usp=sharing",
+    "https://drive.google.com/file/d/1qdpMsurDAVRhvHkNsHcwabgi82-rvBhK/view?usp=sharing",
 };
