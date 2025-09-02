fa0df6fadeafe3a984d41a2856aebc5d489dadbd
estanho <estanhoeu@gmail.com>
2025-09-02 19:56:40 -0300

    feat: update alert

--- src/data/alert.ts
+++ src/data/alert.ts
@@ -7,7 +7,7 @@ interface Alert {
 
 export const alert: Alert = {
   enable: true,
-  title: "Servidor Offline!",
+  title: "Servidor Online para testes!",
   description:
     "O servidor está com um erro que estamos tentando identificar ainda junto com a equipe do EnxadaHost. Pedimos desculpas pelo inconveniente.",
   duration: 60000,
