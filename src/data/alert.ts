75775cadc3ae4397a06568deb1f1dfd5c965bd50
estanho <estanhoeu@gmail.com>
2025-09-01 23:45:46 -0300

    feat: add alert interface

--- src/data/alert.ts
+++ src/data/alert.ts
@@ -1,7 +1,14 @@
-export const alert = {
+interface Alert {
+  enable: boolean;
+  title: string;
+  description: string;
+  duration: number;
+}
+
+export const alert: Alert = {
   enable: true,
   title: "Servidor Offline!",
   description:
     "O servidor está com um problema que estamos tentando identificar ainda. Peço que aguardem.",
-  duration: 30000,
+  duration: 60000,
 };
