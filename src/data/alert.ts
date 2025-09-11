de2f72a5eb3549ba82fdfaf4d317f291ae1e7ba1
estanho <estanhoeu@gmail.com>
2025-09-11 18:38:21 -0300

    feat: update alert message

--- src/data/alert.ts
+++ src/data/alert.ts
@@ -1,14 +1,14 @@
-interface Alert {
+interface IAlert {
   enable: boolean;
   title: string;
   description: string;
   duration: number;
 }
 
-export const alert: Alert = {
+export const alert: IAlert = {
   enable: true,
   title: "Servidor está passando por algumas mudanças!",
   description:
-    "Estamos verificando a possibilidade de atualizar/refazer o modpack para melhorar o desempenho do servidor. Infelizmente até conseguirmos corrigir todos os problemas, o servidor vai continuar com bastante instabilidade. Pedimos desculpas pelo inconveniente.",
+    "Estamos tentando resolver os problemas relacionados aos fechamentos inesperados do servidor. Infelizmente podem ocorrer algumas instabilidades até tudo ser resolvido. Pedimos desculpas pelo inconveniente.",
   duration: 30_000,
 };
