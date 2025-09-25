8a0927382142d4d8db601ddf10169614c3dcf2b7
estanho <estanhoeu@gmail.com>
2025-09-25 18:57:07 -0300

    feat: disable alert

--- src/data/alert.ts
+++ src/data/alert.ts
@@ -6,7 +6,7 @@ interface IAlert {
 }
 
 export const alert: IAlert = {
-  enable: true,
+  enable: false,
   title: "Servidor está passando por algumas mudanças!",
   description:
     "Estamos tentando resolver os problemas relacionados aos fechamentos inesperados do servidor. Infelizmente podem ocorrer algumas instabilidades até tudo ser resolvido. Pedimos desculpas pelo inconveniente.",
