d8d6cea7f6362bcd614dcc806fe89303df720ef3
estanho <estanhoeu@gmail.com>
2025-10-04 06:24:44 -0300

    feat: update

--- src/data/download.ts
+++ src/data/download.ts
@@ -12,5 +12,5 @@ export const download: IDownload = {
     "https://drive.google.com/file/d/1HVrXfO7Ohzz436gkQ4ssdL7nuDWJ6B78/view?usp=sharing",
   download_mods_url:
     "https://drive.google.com/file/d/1Y8LSt20FadL7ZN6r4IOePQrrBgS3vzT9/view?usp=sharing",
-  number_of_mods: 108,
+  number_of_mods: 107,
 };
